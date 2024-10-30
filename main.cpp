d2bea8cfc73c985eb2804015316fccf0c3689d55
Antoni <149586012+AntoniJowsa@users.noreply.github.com>
2024-10-30 20:50:25 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,51 +15,13 @@ void InitializeConsole() {
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
     InitializeConsole();
 
-    SetConsoleTitleW(L"aBLOX - Activating");
-    std::cout << R"(
-           #******#           
-         #**********#         
-       %**************#       
-     #******************#     
-   ##*********************#   
- %#********##    %#********#% 
-#*********#        #*********#
-#********#          #********#
-#********#          #********#
-#*********#        #*********#
- %#********##    %#********#% 
-   ##*********************#   
-     #******************#     
-       %**************#       
-         #**********#         
-           #******#                                                  
-    )" << "\n";
+    SetConsoleTitleW(L"aBLOX 1.1-S - Activating");
 
-    std::cout << "aBLOX - Activating\n";
-    system("cls");
+    std::cout << "aBLOX 1.1-S - Activating\n";
     HANDLE robloxMutex = CreateMutexW(NULL, true, L"ROBLOX_singletonMutex");
-    SetConsoleTitleW(L"aBLOX - Activated");
-
-    std::cout << R"(
-           #******#           
-         #**********#         
-       %**************#       
-     #******************#     
-   ##*********************#   
- %#********##    %#********#% 
-#*********#        #*********#
-#********#          #********#
-#********#          #********#
-#*********#        #*********#
- %#********##    %#********#% 
-   ##*********************#   
-     #******************#     
-       %**************#       
-         #**********#         
-           #******#                                                  
-    )" << "\n";
-    
-    std::cout << "aBLOX - Activated";
+    system("cls");
+    SetConsoleTitleW(L"aBLOX 1.1-S - Activated");
+    std::cout << "aBLOX 1.1-S - Activated";
     
     while (true) {
         Sleep(1000);
